04f5b1547d1ad81b9dc21b9ba30ff5f9f83e96d0
matthewsotoudeh <matthewsot@outlook.com>
2015-01-10 23:19:16 -0800

    added the nav to the landing page too

---
+++ Content/Scripts/nav.js
@@ -0,0 +1,8 @@
+﻿document.getElementById("hamburger").onclick = function () {
+    var body = document.getElementsByTagName("body")[0];
+    if (body.className === "nav-opened") {
+        body.className = "";
+    } else {
+        body.className = "nav-opened";
+    }
+};
