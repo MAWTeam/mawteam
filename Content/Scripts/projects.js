51c64c7d24e298dde4e5c72a157d096290339f00
matthewsotoudeh <matthewsot@outlook.com>
2015-07-04 22:17:57 -0700

    the more info overlay exists

--- Content/Scripts/projects.js
+++ Content/Scripts/projects.js
@@ -1,8 +1,3 @@
-$("#projects .info .button").click(function () {
-    var article = $(this).closest("article");
-    
-    $("#more-info-wedge h1").text(article.find("h1").text());
-    $("#more-info-wedge p").text(article.find("p").text());
-    
-    wedge.show("#more-info-wedge");
+$("#projects").on("click", ".button:first-child", function () {
+    wedge.show("#more-info-wedge", { "opacity": 0.7 });
 });
