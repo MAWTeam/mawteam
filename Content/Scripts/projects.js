d2c9b29f36c8d613c48732c3ff7ee3eeed03547a
matthewsotoudeh <matthewsot@outlook.com>
2015-07-04 22:22:25 -0700

    better info overlays

--- Content/Scripts/projects.js
+++ Content/Scripts/projects.js
@@ -1,3 +1,6 @@
 $("#projects").on("click", ".button:first-child", function () {
+    $("#more-info-wedge h1").text($(this).closest("article").find("h1").text());
+    $("#more-info-wedge p").text($(this).closest("article").find("p").text());
+    $("#more-info-wedge .timeline").text($(this).closest("article").attr("data-timeline"));
     wedge.show("#more-info-wedge", { "opacity": 0.7 });
 });
